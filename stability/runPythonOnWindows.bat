af611fa8510f32556c3abcd0b58ad8200a953b1d
Drew Scoggins <anscoggi@microsoft.com>
2017-02-09 14:21:15 -0800

    Add upload step

--- stability/runPythonOnWindows.bat
+++ stability/runPythonOnWindows.bat
@@ -1,6 +1,5 @@
 py %WORKSPACE%\stability\stability\windows_native-stability-test.py --stabilization --iterations=10
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-echo F | xcopy stability.csv C:\stability.csv
 py C:\Tools\Microsoft.BenchView.JSONFormat\tools\submission-metadata.py --name "%COMPUTERNAME% Stability Run %mydate%" --user-email "dotnet-bot@microsoft.com"
 py C:\Tools\Microsoft.BenchView.JSONFormat\tools\build.py git --type rolling --branch master
 py C:\Tools\Microsoft.BenchView.JSONFormat\tools\machinedata.py
@@ -15,3 +14,4 @@ py C:\Tools\Microsoft.BenchView.JSONFormat\tools\submission.py measurement.json 
                                                                --config Computer "%COMPUTERNAME%" ^
                                                                --architecture x64 ^
                                                                --machinepool "perfsnake"
+py C:\Tools\Microsoft.BenchView.JSONFormat\tools\upload.py submission.json --container stability
