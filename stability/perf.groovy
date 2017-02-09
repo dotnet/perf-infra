5b4a6c43b086e2683600fe6f07322d4852c94d9d
Drew Scoggins <anscoggi@microsoft.com>
2017-02-08 17:29:59 -0800

    Add stability tests
    This is the initial commit of the stability work that had been in the
    citest repo.  This allows us to do stability testing on our perf
    machines.

---
+++ stability/perf.groovy
@@ -0,0 +1,47 @@
+// Import the utility functionality.
+import jobs.generation.*;
+
+def project = GithubProject
+def branch = GithubBranchName
+def gitUrl = Utilities.calculateGitURL(project)
+
+// Define the nightly workflows that measure the stability of performance machines
+// Put these in a folder
+
+def stabilityTestingFolderName = 'stability_testing'
+folder('stability_testing') {}
+
+// Defines stability testing for all linux OS's.  It might be possible to unify this into all unixes.
+// Can't use pipeline here.  The postbuild step that is used to launch the subsequent builds doesn't work
+// with the pipeline project type.  It might be possible to do this other ways.  For instance, evaluating all
+// all eligible nodes based on an expression in a pipeline job and launching downstream jobs.
+
+// We run two types:
+// 1) Native testing of known binaries (a few parsec benchmarks) that are very stable.
+// 2) Managed testing of known binaries that are stable
+
+['windows'].each { osFamily ->
+    def nativeStabilityJob = job(Utilities.getFullJobName("${osFamily}_native_stability_test", false, stabilityTestingFolderName)) {
+        // Add a parameter to specify which nodes to run the stability job across
+        parameters {
+            labelParam('NODES_LABEL') {
+                allNodes('allCases', 'AllNodeEligibility')
+                defaultValue("${osFamily}_clr_perf")
+                description('Nodes label expression to run the unix stability job across')
+            }
+        }
+        steps {
+            if (osFamily == 'windows') {
+                batchFile('runPythonOnWindows.bat')
+            }
+            else {
+                shell("stability/linux_native-stability-test.py")
+            }
+        }
+    }
+
+    // Standard job setup, etc.
+    Utilities.standardJobSetup(nativeStabilityJob, project, false, "*/${branch}")
+    // Set the cron job here.  We run nightly on each flavor, regardless of code changes
+    Utilities.addPeriodicTrigger(nativeStabilityJob, "@daily", true /*always run*/)
+}
